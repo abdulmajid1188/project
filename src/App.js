f7782b2d1a192146a26c6e92f1208ebe5e79d922
abdulmajid1188 <88856252+abdulmajid1188@users.noreply.github.com>
2021-08-15 23:48:41 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import Navbar from "./components/layout/Navbar";
 import {
   BrowserRouter as Router,
